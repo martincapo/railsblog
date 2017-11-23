88d4db23d59cef184508a03bde85d7bdab60028d
Martin Kim <martinkim7677@gmail.com>
2017-11-22 21:02:03 -0500

    First commit

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  scope :most_recent, -> { order(id: :desc) }
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+
+  def display_day_published
+    "Published #{created_at.strftime('%-b %-d %-y')}"
+  end
+end
